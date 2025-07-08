0d83b7512ea862deccc8dd03a8da6ad0895deed7
Tony Ferreira <email.tferreira@gmail.com>
2025-07-08 06:58:50 -0700

    Archive old powershell profile

--- powershell/Microsoft.PowerShell_profile_orig.ps1
+++ powershell/Microsoft.PowerShell_profile_orig.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/avit.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/avit.omp.json" | Invoke-Expression
 # Invoke-Expression (&starship init powershell)
 
 Import-Module PSReadLine
